28c2748fca9a9038d9d8beb2ae80068048628569
Ravi Kuntal <kuntalravi43@gmail.com>
2025-10-29 14:22:46 +0530

    Initial project setup

---
+++ app/src/main/java/com/rk/pace/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rk.pace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.rk.pace.ui.theme.PaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PaceTheme {}
+        }
+    }
+}
